df5f9cad886ee9a47bc6af51f3724d7097336f9f
cpa4001 <cpapostoli4001@eagle.fgcu.edu>
2021-10-01 22:54:19 -0400

    Added All files from DadJokeML

---
+++ Hello.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+   if request.method == 'POST':
+      if request.form.get('action1') == 'GIVE ME A DAD JOKE!!':
+         render_template('home.html')
+##      elif  request.form.get('action2') == 'VALUE2':
+##         pass # do something else
+##      else:
+##         pass # unknown
+##   elif request.method == 'GET':
+##      return render_template('home.html', form=form)
+   return render_template('home.html')
+
+@app.route('/christian')
+def christian():
+   return 'This is Christian\'s  route'
+
+@app.route('/about')
+def about():
+   return render_template("about.html")
+
+@app.route('/button')
+def button():
+   return render_template("button.html")
+   
+if __name__ == '__main__':
+   app.run(debug=True)
+   
